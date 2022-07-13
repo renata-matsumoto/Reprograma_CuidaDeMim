aafc408686e2ea804844c609f1199090aced733d
renata-matsumoto <renatas.matsumoto@gmail.com>
2022-07-12 23:55:33 -0300

    Subindo arquivos do Projeto

---
+++ src/app.js
@@ -0,0 +1,10 @@
+require("dotenv-safe").config();
+const express = require("express");
+const app = express()
+const cors = require("cors");
+
+app.use(express.json())
+app.use(cors());
+
+
+module.exports = app;
