55351b52133a27e7ef362fb66d573069c7dea360
renata-matsumoto <renatas.matsumoto@gmail.com>
2022-07-26 08:08:45 -0300

    add swagger file and update app.js

--- src/app.js
+++ src/app.js
@@ -1,5 +1,7 @@
 require("dotenv-safe").config();
 const express = require("express");
+const swaggerUi = require('swagger-ui-express')
+const swaggerFile = require('../swagger_output.json')
 const cors = require("cors");
 const db = require('./database/mongoConfig');
 const app = express()
@@ -10,12 +12,11 @@ app.use(cors());
 
 const idosoRotas = require('./routes/idosoRoutes')
 const voluntarioRotas = require('./routes/voluntarioRoutes')
-// const usuarioRotas = require("./routes/usuarioRoutes");
 const index = require("./routes/indexRoutes");
 
 app.use("/idoso", idosoRotas)
 app.use("/voluntario", voluntarioRotas )
-// app.use("/usuario", usuarioRotas)
 app.use("/", index);
+app.use('/doc', swaggerUi.serve, swaggerUi.setup(swaggerFile))
 
 module.exports = app;
